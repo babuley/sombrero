5065e23658683ad60163755ef9ed1c796990f362
adams <a4szczepaniak@gmail.com>
2020-12-10 16:47:51 +0000

    More tests with H2 solver
    Signed-off-by: adams <a4szczepaniak@gmail.com>

---
+++ src/test/resources/solveProblem.sh
@@ -0,0 +1,3 @@
+curl  -i -X POST http://localhost:8080/timeTable/solve  -H "Content-Type:application/json"
+
+
---
+++ src/test/resources/submitProblem.sh
@@ -0,0 +1,3 @@
+curl  -i -X POST -d @problemSpace3.json http://localhost:8080/timeTable/submitProblem  -H "Content-Type:application/json"
+
+
